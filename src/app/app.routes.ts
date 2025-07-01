39a6e8464bca74b24e12a35f77cd6837871cf6a7
Viram7 <kushwahavicky15@gmail.com>
2025-07-01 14:27:18 +0530

    3 commit

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -6,7 +6,7 @@ import { WorkshopComponent } from './workshop/workshop.component';
 export const routes: Routes = [
 
   {path:'',component:HomeComponent },
-  {path:'',redirectTo:'home', pathMatch: 'full' },
+  // {path:'',redirectTo:'home', pathMatch: 'full' },
 
   {path:'home',component:HomeComponent },
   {path:'about',component:AboutComponent},
